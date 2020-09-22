c90fb88f970387b39d171096e9fbe1ff1e50abf0
Acasune <tttt4aaaa@gmail.com>
2020-09-22 20:41:48 +0900

    added logos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,8 @@ import baseStyle from "../styles/base-style"
 import Layout from "../components/layout"
 import { StyledBackground } from "../internal/index/background"
 import { StyledHeroImage } from "../internal/index/hero-image"
-import ProfileCvMDX from "../internal/index/profile-cv.mdx"
 import DetailModal from "../internal/contents/detail-modal.tsx"
+import ProfileCvMDX from "../internal/index/profile-cv.mdx"
 import SkillsMDX from "../internal/index/skills.mdx"
 import WorksMDX from "../internal/index/works.mdx"
 import LikesMDX from "../internal/index/likes.mdx"
@@ -14,6 +14,7 @@ import DetailAboutMDX from "../internal/details/about-this-website.mdx"
 import DetailProfileCvMDX from "../internal/details/profile-and-cv.mdx"
 import DetailSkillsMDX from "../internal/details/skills.mdx"
 import DetailLikesMDX from "../internal/details/likes.mdx"
+import DetailContactMDX from "../internal/details/contact.mdx"
 
 export default () => {
   return (
@@ -38,7 +39,7 @@ export default () => {
       </DetailModal>
       <ContactMDX />
       <DetailModal>
-        <DetailAboutMDX />
+        <DetailContactMDX />
       </DetailModal>
       <StyledBackground />
     </Layout>
